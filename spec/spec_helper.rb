9fd27e0c8268cb26829350c3134885cec4ccbdf7
Rhett Sutphin <rhett@detailedbalance.net>
2011-06-18 16:27:16 -0500

    Remove unused helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,18 +9,6 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require 'handbrake'
 
 RSpec.configure do |config|
-  # Captures everything printed to stdout during the block
-  # and returns it as a string.
-  def capture_stdout
-    old_stdout, $stdout = $stdout, StringIO.new
-    begin
-      yield
-      $stdout.string
-    ensure
-      $stdout = old_stdout
-    end
-  end
-
   def tmpdir(sub=nil)
     @tmpdir ||= begin
                   dirname = File.expand_path("../tmp", __FILE__)
